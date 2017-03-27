09d777e5598a8ddef717f99386a323b85b116630
Robbe De Geyndt <robbe.degeyndt@student.odisee.be>
2017-03-27 14:49:30 +0200

    Initial Commit

---
+++ Inventory/www/scripts/index.js
@@ -0,0 +1,16 @@
+﻿$("#scan").click(function ($event) {
+    //prevent default
+    $event.preventDefault();
+
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            $("#result").text("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+        },
+        function (error) {
+            $("#result").text("Scanning failed: " + error);
+        }
+    );
+});
