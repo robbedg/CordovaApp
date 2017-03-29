23f44ddfd9c667299e8667f338e24df3d2779493
Robbe De Geyndt <robbe.degeyndt@student.odisee.be>
2017-03-29 13:58:25 +0200

    Ability to store items.

--- Inventory/www/scripts/index/index.js
+++ Inventory/www/scripts/index/index.js
@@ -19,8 +19,10 @@ $("#scan").click(function ($event) {
             //show results
             $("#results").removeClass('hidden');
 
-            //save to file
-            window.localStorage.setItem('test', JSON.stringify($info));
+            //add data to hidden fields
+            $("#item_id").val($info[0]);
+            $("#location").val($info[1]);
+            $("#category").val($info[2]);
         },
         function ($error) {
             $("#result").text("Scanning failed: " + $error);
@@ -28,14 +30,63 @@ $("#scan").click(function ($event) {
     );
 });
 
+$("#save a").click(function ($event) {
+    //prevent default
+    $event.preventDefault();
+
+    //get date from
+    var $dateStringFrom = $("#datetimepicker_from").datetimepicker('date');
+    var $from = moment($dateStringFrom).format();
+
+    //get date until
+    var $dateStringUntil = $("#datetimepicker_until").datetimepicker('date');
+    var $until = moment($dateStringUntil).format();
+
+    //save to localstorage
+    console.log(localStorage.getItem('items'));
+    var $items = JSON.parse(localStorage.getItem('items'));
+    console.log($items);
+
+    $items.push({ 'id': $("#item_id").val(), 'location': $("#location").val(), 'category': $("#category").val(), 'from': $from, 'until': $until });
+
+    localStorage.setItem('items', JSON.stringify($items));
+    console.log(localStorage.getItem('items'));
+
+    //get items
+    getItems();
+});
+
+//on document load
 $(document).ready(function () {
+    //localStorage.clear();
+    //localStorage.setItem('items', JSON.stringify([]));
     //localStorage.setItem('items', JSON.stringify([{ 'id': '0', 'location': 'Location1', 'category': 'Bank' }]));
     getItems();
 });
 
+//get items from localstorage
 function getItems() {
     if (localStorage.getItem('items') !== null) {
         var $items = JSON.parse(localStorage.getItem('items'));
-        console.log($items);
+
+        //empty table
+        $("#table tbody").empty();
+
+        //fill table
+        $($items).each(function ($index, $element) {
+            $("#table tbody")
+                .append($('<tr />')
+                    .append($('<td />').append($element['id']))
+                    .append($('<td />').append($element['location']))
+                    .append($('<td />').append($element['category']))
+                    .append($('<td />').append($element['from']))
+                    .append($('<td />').append($element['until']))
+                );
+        });
+
+
+    } else {
+        //make new empty item array
+        localStorage.setItem('items', JSON.stringify([]));
     }
 }
