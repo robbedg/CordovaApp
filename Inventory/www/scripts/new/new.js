c2e232a5eefe8882d368f7a988b1936be4a01233
Robbe De Geyndt <robbe.degeyndt@student.odisee.be>
2017-04-18 12:07:05 +0200

    Added JS & CSS file for add page. Issue #3

---
+++ Inventory/www/scripts/new/new.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+
+$(document).ready(function () {
+
+    //when item is selected, add ability to add attributes
+    $("#what_select").change(function () {
+        var $option = $(this);
+
+        if ($option.val() === 'item') {
+            $("#add-attributes").removeClass('hidden');
+        } else {
+            $("#add-attributes").addClass('hidden');
+        }
+    });
+});
