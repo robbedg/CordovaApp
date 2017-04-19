72616307e193d321cb932cc6b08657b672f86245
Robbe De Geyndt <robbe.degeyndt@student.odisee.be>
2017-04-19 11:52:13 +0200

    Added ability to push changes to main application. Issue #1

---
+++ Inventory/www/scripts/synchronize/download.js
@@ -0,0 +1,43 @@
+﻿"use strict";
+
+$("#download a").click(function ($event) {
+    //prevent default
+    $event.preventDefault();
+
+    //get address
+    if (localStorage.getItem('settings') !== null) {
+        var $settings = JSON.parse(localStorage.getItem('settings'));
+        //get data
+        pullData($settings.address);
+    } else {
+        window.location('settings.html');
+    }
+});
+
+function pullData($address) {
+
+    //get database connection
+    var $db = getDB();
+
+    //ajax get data
+    $.ajax({
+        url: $address + '/index.php/synchronize/download',
+        crossDomain: true,
+        type: 'GET'
+    }).done(function ($response) {
+        //locations
+        $db.locations.clear().then(function () {
+            $db.locations.bulkAdd($response['locations']['data']);
+        });
+
+        //categories
+        $db.categories.clear().then(function () {
+            $db.categories.bulkAdd($response['categories']['data']);
+        });
+
+        //items
+        $db.items.clear().then(function () {
+            $db.items.bulkAdd($response['items']['data']);
+        })
+    });
+}
---
+++ Inventory/www/scripts/synchronize/upload.js
@@ -0,0 +1,75 @@
+﻿"use strict";
+
+/**
+ * Click upload button
+**/
+$("#upload a").click(function ($event) {
+    //prevent default
+    $event.preventDefault();
+
+    //get address
+    if (localStorage.getItem('settings') !== null) {
+        var $settings = JSON.parse(localStorage.getItem('settings'));
+        //push data
+        pushData($settings.address);
+    } else {
+        window.location('settings.html');
+    }
+});
+
+/**
+ * Push data to main application
+**/
+function pushData($address) {
+
+    //get database connection
+    var $db = getDB();
+
+    //object for data
+    var $data = new Object();
+
+    //get data from local DB
+
+    //1st of chain
+    getLocations();
+
+    //locations
+    function getLocations() {
+        $db.locations_out.toArray(function ($locations) {
+            $data['locations'] = $locations;
+        }).then(function () {
+            //2nd of chain
+            getCategories()
+        });
+    }
+
+    function getCategories() {
+        $db.categories_out.toArray(function ($categories) {
+            $data['categories'] = $categories;
+        }).then(function () {
+            //3rd of chain
+            getItems();
+        });
+    }
+
+    function getItems() {
+        $db.items_out.toArray(function ($items) {
+            $data['items'] = $items;
+        }).then(function () {
+            //start upload
+            upload($data);
+        });
+    }
+
+    function upload($data) {
+        $.ajax({
+            url: $address + '/index.php/synchronize/upload',
+            crossDomain: true,
+            type: 'POST',
+            dataType: 'json',
+            data: JSON.stringify($data)
+        }).done(function ($response) {
+            console.log($response);
+        });
+    }
+}
